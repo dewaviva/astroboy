7e6eb27d52918bbd570825c79133ddd532a32111
dewaviva <dewaviva70@gmail.com>
2025-07-13 03:35:25 +0700

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,31 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from "@tailwindcss/vite";
 import preact from "@astrojs/preact";
-import sitemap from "@astrojs/sitemap"
+import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://neonmint.efeele.dev",
-  integrations: [preact(), icon(), sitemap({
-    filter: (page) =>
-      !page.includes("/blog/tags") &&
-      !page.includes("/blog/techs"),
-  }),],
+  output: 'server', // <--- ini penting buat SSR
+  adapter: cloudflare(), // <--- ini juga WAJIB biar Cloudflare tau ini SSR
+
+  site: "https://akun-gacor.com",
+
+  integrations: [
+    preact(),
+    icon(),
+    sitemap({
+      filter: (page) =>
+        !page.includes("/blog/tags") &&
+        !page.includes("/blog/techs"),
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
   },
+
   markdown: {
     shikiConfig: {
       theme: 'github-dark'
